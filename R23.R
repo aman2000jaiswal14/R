d38f544432d458557e0c522320a3faf52542e713
AMAN JAISWAL <aman2000jaiswal14@gmail.com>
2020-06-28 00:05:53 +0530

    READING FROM API
    oauth_app, sign_ouauth1.0, GET, content, fromJSON, toJSON

---
+++ R23.R
@@ -0,0 +1,11 @@
+## Twitter api
+# www.dev.twitter.com/apps
+
+# myapp = oauth_app("twitter",key="consumerkey",secret="consumersSecret")
+# sig = sign_oauth1.0(myapp,token="tokenhere",token_secret="tokensecret")
+# homeTL = GET("https://api.twitter.com/1.1/status/home_timeline.json",sig)
+
+# json1 = content(homeTL)
+# json2= jsonlite:: fromJSON(toJSON(json1))
+# print(json2[,1:4])
+
